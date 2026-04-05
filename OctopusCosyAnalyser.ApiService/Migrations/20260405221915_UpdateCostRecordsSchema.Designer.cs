561bfbc5f82f9bae20d11762dbbf8da647eb3d3d
Rob Hutchin <rob.hutchin@iglu.com>
2026-04-05 23:22:42 +0100

    db

--- OctopusCosyAnalyser.ApiService/Migrations/20260405221915_UpdateCostRecordsSchema.Designer.cs
+++ OctopusCosyAnalyser.ApiService/Migrations/20260405221915_UpdateCostRecordsSchema.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using OctopusCosyAnalyser.ApiService.Data;
@@ -11,9 +12,11 @@ using OctopusCosyAnalyser.ApiService.Data;
 namespace OctopusCosyAnalyser.ApiService.Migrations
 {
     [DbContext(typeof(CosyDbContext))]
-    partial class CosyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260405221915_UpdateCostRecordsSchema")]
+    partial class UpdateCostRecordsSchema
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ OctopusCosyAnalyser.ApiService/Migrations/20260405221915_UpdateCostRecordsSchema.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OctopusCosyAnalyser.ApiService.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateCostRecordsSchema : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalUsageKwh",
+                table: "DailyCostRecords",
+                type: "numeric(18,6)",
+                precision: 18,
+                scale: 6,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "TotalCostPence",
+                table: "DailyCostRecords",
+                type: "numeric(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "StandingChargePence",
+                table: "DailyCostRecords",
+                type: "numeric(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: true,
+                oldClrType: typeof(double),
+                oldType: "double precision",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "AvgUnitRatePence",
+                table: "DailyCostRecords",
+                type: "numeric(18,4)",
+                precision: 18,
+                scale: 4,
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "double precision");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalUsageKwh",
+                table: "DailyCostRecords",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,6)",
+                oldPrecision: 18,
+                oldScale: 6);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "TotalCostPence",
+                table: "DailyCostRecords",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,4)",
+                oldPrecision: 18,
+                oldScale: 4);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "StandingChargePence",
+                table: "DailyCostRecords",
+                type: "double precision",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,4)",
+                oldPrecision: 18,
+                oldScale: 4,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<double>(
+                name: "AvgUnitRatePence",
+                table: "DailyCostRecords",
+                type: "double precision",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(18,4)",
+                oldPrecision: 18,
+                oldScale: 4);
+        }
+    }
+}
